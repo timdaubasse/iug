f3ecdbb4641efc46a272c08d879fd140a72dd6db
Timothé <daubtimd@yahoo.fr>
2016-10-14 14:58:38 +0200

    Ajout des tests polyline

--- tests/tp3.c
+++ tests/tp3.c
@@ -26,31 +26,62 @@ int ei_main(int argc, char** argv){
 	start.x = 320, start.y = 240;
 	hw_surface_lock(main_window);
 	ei_fill(main_window, &c, EI_FALSE);
+	
 	c.red = 0x00, c.green = 0x00, c.blue = 0x00, c.alpha = 0x00;
+	
 	end.x = 220, end.y = 240;
 	ei_draw_line(main_window, start, end, c);
+	
 	end.x = 220, end.y = 190;
 	ei_draw_line(main_window, start, end, c);
+	
 	end.x = 220, end.y = 140;
 	ei_draw_line(main_window, start, end, c);
+	
 	end.x = 270, end.y = 140;
 	//ei_draw_line(main_window, start, end, c);
+	
 	end.x = 320, end.y = 140;
 	//ei_draw_line(main_window, start, end, c);
+	
 	end.x = 370, end.y = 140;
 	//ei_draw_line(main_window, start, end, c);
+	
 	end.x = 420, end.y = 140;
 	//ei_draw_line(main_window, start, end, c);
+	
 	end.x = 420, end.y = 190;
 	ei_draw_line(main_window, start, end, c);
+	
 	end.x = 420, end.y = 240;
 	ei_draw_line(main_window, start, end, c);
+	
 	end.x = 420, end.y = 290;
 	ei_draw_line(main_window, start, end, c);
+	
 	end.x = 420, end.y = 340;
 	ei_draw_line(main_window, start, end, c);
+	
 	end.x = 370, end.y = 340;
 	//ei_draw_line(main_window, start, end, c);
+
+	ei_linked_point_t* first_point;
+	ei_linked_point_t* nd_point;
+	ei_linked_point_t* rd_point;
+	first_point->point.x=50;
+	first_point->point.y=100;	
+	nd_point->point.x=150;
+	nd_point->point.y=100;
+	rd_point->point.x=150;
+	rd_point->point.y=180;
+	first_point->next=nd_point;
+	nd_point->next=rd_point;
+
+	ei_draw_polyline(surface,first_point,c)
+	
+	
+
+	
 	hw_surface_unlock(main_window);
 
 	hw_surface_update_rects(NULL);
